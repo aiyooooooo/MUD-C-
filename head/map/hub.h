2a743189f15fa5cfc022c6f6e49a7f0d2380bea0
aiyooooooo <5703114@gmail.com>
2017-08-30 11:09:31 +0800

    Add files via upload

---
+++ head/map/hub.h
@@ -0,0 +1,57 @@
+#ifndef HUB_H
+#define HUB_H
+
+#include "head/map/map.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Pub
+{
+public:
+    void show_Pub();
+    void get_soldior();
+private:
+    //Shero shero
+};
+
+void Pub::get_soldior()
+{
+
+}
+
+void Pub:: show_Pub()
+{
+    char choose[500];
+    int mark = 1;
+
+    while(mark){
+        gets(choose);
+        if(strlen(choose) > 1){
+            continue;
+        }
+        switch(choose[0]){
+            case '1':
+                mark = 1;
+                get_soldior();
+                break;
+            case '2':
+                mark = 1;
+                //shero.talk;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
+
+int main()
+{
+
+
+    return 0;
+}
+
+#endif HUB_H
